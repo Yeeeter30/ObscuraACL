840f544ab442356ec63e5a9dbca1aeba4a10a8dd
PirxcyFinal <120330677+Yeeeter30@users.noreply.github.com>
2024-10-31 16:43:19 -0400

    Create Script paste.lua

---
+++ Script paste.lua
@@ -0,0 +1,6 @@
+Settings = {
+    CatBypasser = false,     -- Set to true to load Cat Bypasser
+    InfiniteYield = true    -- Set to true to load Infinite Yield
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Yeeeter30/ObscuraACL/refs/heads/main/Obscura.lua",true))()
