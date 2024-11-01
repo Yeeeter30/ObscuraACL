31c8c32454d23409cb751965644671c3e572ff89
PirxcyFinal <120330677+Yeeeter30@users.noreply.github.com>
2024-11-01 14:42:13 -0400

    Update Script paste.lua

--- Script paste.lua
+++ Script paste.lua
@@ -1,6 +1,10 @@
-Settings = {
-    CatBypasser = false,     -- Set to true to load Cat Bypasser
-    InfiniteYield = true    -- Set to true to load Infinite Yield
+local Settings = {
+    CatBypasser = false,
+    InfiniteYield = false,
+    AnonymousMode = false, -- Set to true for anonymous mode
+    EnableButton = false, -- Set to true to enable the toggle button
+    ObscuraOnDefault = true, -- Set to true to enable Anti Chat Logger by default
+    AdonisBypass = true -- Set to true to enable Adonis bypass
 }
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Yeeeter30/ObscuraACL/refs/heads/main/Obscura.lua",true))()
